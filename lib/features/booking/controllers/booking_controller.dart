e67d6c3f89736b1d28e2db8156b1c4048559ccf6
makbulinsan04 <makbulinsan04@gmail.com>
2026-07-21 19:08:01 +0700

    feat : perbaikan callback pada midtrans

--- lib/features/booking/controllers/booking_controller.dart
+++ lib/features/booking/controllers/booking_controller.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../services/booking_service.dart';
 
-class BookingController extends GetxController with StateMixin<List<dynamic>> {
+class BookingController extends GetxController with StateMixin<List<dynamic>>, WidgetsBindingObserver {
   final BookingService _service = BookingService();
 
   final selectedDate = ''.obs;
@@ -12,11 +13,27 @@ class BookingController extends GetxController with StateMixin<List<dynamic>> {
   final bookingHistory = <dynamic>[].obs;
   final slotStatus = Rx<RxStatus>(RxStatus.empty());
   final formStatus = Rx<RxStatus>(RxStatus.empty());
+  bool _awaitingPaymentReturn = false;
 
   @override
   void onInit() {
     super.onInit();
+    WidgetsBinding.instance.addObserver(this);
     fetchHistory();
+  }
+
+  @override
+  void onClose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.onClose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed && _awaitingPaymentReturn) {
+      _awaitingPaymentReturn = false;
+      fetchHistory();
+    }
   }
 
   Future<void> fetchAvailability(int lapanganId, String tanggal) async {
@@ -65,16 +82,17 @@ class BookingController extends GetxController with StateMixin<List<dynamic>> {
 
       final snapUrl = res.data['snap_url']?.toString();
       formStatus.value = RxStatus.success();
+      selectedSlots.clear();
 
       if (snapUrl != null && snapUrl.isNotEmpty) {
+        _awaitingPaymentReturn = true;
         final uri = Uri.parse(snapUrl);
         await launchUrl(uri, mode: LaunchMode.externalApplication);
+        Get.back();
       } else {
         Get.snackbar('Booking Berhasil', 'Menunggu pembayaran dikonfirmasi.');
+        fetchHistory();
       }
-
-      selectedSlots.clear();
-      fetchHistory();
     } on DioException catch (e) {
       formStatus.value = RxStatus.error(
         e.response?.data['message']?.toString() ?? 'Gagal membuat booking.',
